0f99f75a4968bdd05058d9ea8787a6dd6aad8bba
Thibaut Géry <thibaut.gery@gmail.com>
2016-07-18 18:51:12 +0200

    add simple parser

---
+++ import/StreamParser.js
@@ -0,0 +1,42 @@
+const iconv = require('iconv-lite');
+const stream = require('stream');
+
+
+class ParseStream extends stream.Transform {
+  constructor(headers) {
+    super({
+      objectMode: true
+    });
+    this.headers = headers
+  }
+
+  _transform(chunk, encoding, callback) {
+    const line = decodeFromBinary(chunk)
+    const fields = line.split('\t');
+    let data = {}
+    Object.keys(this.headers).forEach((key) => {
+      const parseOption = this.headers[key]
+      const rawValue = fields[this.headers[key].position]
+      data[key] = parseField(rawValue, parseOption)
+    });
+    this.push(data)
+    callback();
+  }
+}
+
+
+function decodeFromBinary(data) {
+  return iconv.decode(Buffer.from(data) ,'iso-8859-1')
+}
+
+function parseField(rawValue, parseOption) {
+  switch (parseOption.type) {
+    case 'integer':
+      return parseInt(rawValue)
+    default:
+      return rawValue;
+  }
+
+}
+
+module.exports = ParseStream
---
+++ import/test/parser.test.js
@@ -0,0 +1,62 @@
+const parser = require('../parser')
+const ParserStream = require('../StreamParser')
+const expect = require('chai').expect
+const MemoryStream = require('memorystream');
+const fs = require('fs')
+
+
+describe('the parser file', () => {
+
+  let memStream;
+  beforeEach(() => {
+    memStream = new MemoryStream(undefined, { objectMode: true, writable: true, readable: false});
+  })
+
+  describe('when there is no headers', () => {
+    it('return the empty object', (done) => {
+      const line = "61266250	A 313 200 000 UI POUR CENT, pommade	pommade	cutanée	Autorisation active	Procédure nationale	Commercialisée	12/03/1998			 PHARMA DEVELOPPEMENT	Non"
+      const headers = {}
+      const parserStream = new ParserStream(headers)
+      parserStream
+        .pipe(memStream)
+        .on('finish', () => {
+          expect(memStream.queue[0]).to.deep.equal({});
+          done();
+        })
+      parserStream.write(line)
+      parserStream.end()
+    })
+  })
+
+  describe('when there is a string field', () => {
+    it('return the string', (done) => {
+      const line = "61266250	A 313 200 000 UI POUR CENT, pommade	pommade	cutanée	Autorisation active	Procédure nationale	Commercialisée	12/03/1998			 PHARMA DEVELOPPEMENT	Non"
+      const headers = { nom : {position: 1}}
+      const parserStream = new ParserStream(headers)
+      parserStream
+        .pipe(memStream)
+        .on('finish', () => {
+          expect(memStream.queue[0]).to.deep.equal({nom: 'A 313 200 000 UI POUR CENT, pommade' });
+          done();
+        })
+      parserStream.write(line)
+      parserStream.end()
+    })
+  })
+
+  describe('when there is a integer field', () => {
+    it('return the string', (done) => {
+      const line = "61266250	A 313 200 000 UI POUR CENT, pommade	pommade	cutanée	Autorisation active	Procédure nationale	Commercialisée	12/03/1998			 PHARMA DEVELOPPEMENT	Non"
+      const headers = { id : { position: 0, type: 'integer'}}
+      const parserStream = new ParserStream(headers)
+      parserStream
+        .pipe(memStream)
+        .on('finish', () => {
+          expect(memStream.queue[0]).to.deep.equal({id: 61266250 });
+          done();
+        })
+      parserStream.write(line)
+      parserStream.end()
+    })
+  })
+})
