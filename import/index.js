88aaa23386e39f0d4a9185e54de5951cdf5d4613
Thibaut Géry <thibaut.gery@gmail.com>
2016-08-03 11:22:45 +0200

    add Composition file in import

--- import/index.js
+++ import/index.js
@@ -2,7 +2,7 @@ const insertFile = require('./insertFile');
 const async = require('async')
 
 module.exports = function(callback) {
-  const fileNames = ['CIS_bdpm', 'CIS_CIP_bdpm']
+  const fileNames = ['CIS_bdpm', 'CIS_CIP_bdpm', 'CIS_COMPO_bdpm']
   const files = fileNames.map((item) => {
     return {
       name: item,
