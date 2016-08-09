0e3e17ceaea684d4c1d833d2f1e819c580576214
Thibaut Géry <thibaut.gery@gmail.com>
2016-08-09 19:20:38 +0200

    add deploy script

---
+++ bin/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+artifact_name="meds-db.tar.gz"
+branch="publish"
+artifact_url="https://circleci.com/api/v1/project/sgmap/api-medicaments/latest/artifacts/0//home/ubuntu/api-medicaments/$artifact_name?branch=$branch&filter=successful"
+
+wget -qO- "$artifact_url" | tar xvz
+
+docker-compose -f ./docker-compose-prod.yml pull
+docker-compose -f ./docker-compose-prod.yml rm -f --all
+docker-compose -f ./docker-compose-prod.yml up
