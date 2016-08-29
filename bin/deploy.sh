59e995b88bce3e55de15764afd517a27514393e8
Thibaut Géry <thibaut.gery@gmail.com>
2016-08-29 17:55:25 +0200

    clean scripts

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,12 +2,6 @@
 
 git pull origin master
 
-artifact_name="meds-db.tar.gz"
-branch="master"
-artifact_url="https://circleci.com/api/v1/project/sgmap/api-medicaments/latest/artifacts/0//home/ubuntu/api-medicaments/$artifact_name?branch=$branch&filter=successful"
-
-wget -qO- "$artifact_url" | tar xvz
-
 docker-compose -f ./docker-compose-prod.yml pull
 docker-compose -f ./docker-compose-prod.yml rm -f --all
 docker-compose -f ./docker-compose-prod.yml up -d
