38c8d222fbee8ddbed50d1589ea64f457eb6c22e
pierre-fromager <pf@pier-infor.fr>
2018-01-05 15:55:52 +0100

    bugfix/bin : fix post download to utf8 with diacritics

--- bin/download.sh
+++ bin/download.sh
@@ -1,12 +1,39 @@
 #!/bin/bash
 
-curl "http://base-donnees-publique.medicaments.gouv.fr/telechargement.php?fichier=CIS_bdpm.txt" > data/CIS_bdpm.txt
-curl "http://base-donnees-publique.medicaments.gouv.fr/telechargement.php?fichier=CIS_CIP_bdpm.txt" > data/CIS_CIP_bdpm.txt
-curl "http://base-donnees-publique.medicaments.gouv.fr/telechargement.php?fichier=CIS_COMPO_bdpm.txt" > data/CIS_COMPO_bdpm.txt
-curl "http://base-donnees-publique.medicaments.gouv.fr/telechargement.php?fichier=CIS_HAS_SMR_bdpm.txt" > data/CIS_HAS_SMR_bdpm.txt
-curl "http://base-donnees-publique.medicaments.gouv.fr/telechargement.php?fichier=CIS_HAS_ASMR_bdpm.txt" > data/CIS_HAS_ASMR_bdpm.txt
-curl "http://base-donnees-publique.medicaments.gouv.fr/telechargement.php?fichier=HAS_LiensPageCT_bdpm.txt" > data/HAS_LiensPageCT_bdpm.txt
-curl "http://base-donnees-publique.medicaments.gouv.fr/telechargement.php?fichier=CIS_GENER_bdpm.txt" > data/CIS_GENER_bdpm.txt
-curl "http://base-donnees-publique.medicaments.gouv.fr/telechargement.php?fichier=CIS_CPD_bdpm.txt" > data/CIS_CPD_bdpm.txt
+dlr="curl -s ";
+url="http://base-donnees-publique.medicaments.gouv.fr/telechargement.php?fichier=";
+dirTarget="./data/";
 
-echo "Téléchargement terminé"
+CIS="CIS_bdpm.txt";
+CIS_CIP="CIS_CIP_bdpm.txt";
+CIS_COMPO="CIS_COMPO_bdpm.txt";
+CIS_HAS_SMR="CIS_HAS_SMR_bdpm.txt";
+CIS_HAS_ASMR="CIS_HAS_ASMR_bdpm.txt";
+HAS_LiensPageCT="HAS_LiensPageCT_bdpm.txt";
+CIS_GENER="CIS_GENER_bdpm.txt";
+CIS_CPD="CIS_CPD_bdpm.txt";
+CIS_InfoImportantes="CIS_InfoImportantes.txt";
+
+function dl(){
+    local filename="$1";
+    local charsetConverter="iconv -f iso8859-1 -t utf-8";
+    echo "Download $filename ...";
+    $($dlr $url$filename | $charsetConverter > $dirTarget$filename);
+}
+
+if [ -d "$dirTarget" ]; then
+ 	echo "Download directory is $dirTarget";
+else
+	echo "Create $dirTarget directory";
+	mkdir -p $dirTarget;
+fi
+
+dl $CIS;
+dl $CIS_CIP;
+dl $CIS_COMPO;
+dl $CIS_HAS_SMR;
+dl $CIS_HAS_ASMR;
+dl $HAS_LiensPageCT;
+dl $CIS_GENER;
+dl $CIS_CPD;
+dl $CIS_InfoImportantes;
